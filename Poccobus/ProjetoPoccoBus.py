5683b7a4ccf64e79cc5c3c273b59e2b1b3de2176
Allex Rocha <alexjorocha@gmail.com>
2022-04-23 15:54:51 -0300

    versão finalizada e testada 1

--- Poccobus/ProjetoPoccoBus.py
+++ Poccobus/ProjetoPoccoBus.py
@@ -1,22 +1,33 @@
 import pandas as pd
 from funcoes_uteis import *
 
+try:
+    excluir_arquivo('vendidos.csv')
+    excluir_arquivo('lista_cancel.csv')
+except:
+    pass
+
 while True:
 
     print('###############BEM VINDO AO SISTEMA POCCOBUS #######################\n')
     print('Selecione uma das opções:\n\n ')
 
-    print('(1) - Comprar um Assento')
-
-    print('(10) - Finalizar')
+    print('\t(1) - Comprar um Assento\n'
+          '\t(2) - Cancelar compra\n'
+          '\t(3) - Resetar o sistema\n'
+          '\t(4) - Finalizar')
 
     opcao = int(input())
 
     if opcao == 1:
         dados = ler_dados()
         dados_vendidos = comprar(dados[0], dados[1])
-
-    if opcao == 10:
+    elif opcao == 2:
         dados = ler_dados()
-        finalizar(dados[0], dados[1])
+        cancelar_compra(dados[0], dados[1], dados[2])
+    elif opcao == 3:
+        resetar()
+    elif opcao == 4:
+        dados = ler_dados()
+        finalizar(dados[0], dados[1], dados[2])
         break
--- Poccobus/funcoes_uteis.py
+++ Poccobus/funcoes_uteis.py
@@ -4,24 +4,23 @@ import datetime
 
 
 # Essa função serve para criar e escrever no arquivos .txt
-def escrever_txt(vendidos, assentos):
+def escrever_txt(vendidos, assentos, lista_cancel):
     assentos_disp = []
     string = ''
-    for i in range(ord('A'), ord('A') + assentos.shape[1]):
+    for i in range(ord('A'), ord('A') + assentos.shape[1] - 1):
         for j in range(1, assentos.shape[0]):
             if assentos.at[j, chr(i)] == '▓':
                 assentos_disp.append(chr(i) + str(j))
     try:
-        with open('relacao_assentos.txt', 'r') as arquivo:
+            arquivo = open('relacao_assentos.txt')
             string = arquivo.read()
             if string:
                 posicao = string.find('=')
                 string = string[0:posicao - 1]
                 with open('relacao_assentos.txt', 'w') as arquivo:
                     arquivo.write(string)
-
     except FileNotFoundError as e:
-        print(f'({e}) gerando novo arquivo txt ...')
+        print(f'{e}:gerando novo arquivo txt ...')
 
     with open('relacao_assentos.txt', 'a') as arquivo:
 
@@ -29,24 +28,30 @@ def escrever_txt(vendidos, assentos):
         data = datetime.datetime.now()
         data_formatada = data.strftime('%d/%m/%Y %H:%M')
         arquivo.write(f'Data e hora: {data_formatada}\n\n')
-        arquivo.write('Assentos vendidos: ')
 
-        arquivo.write(', '.join(vendidos) + '\n')
+        if len(vendidos) > 0:
+            arquivo.write('Assentos vendidos: ')
+            arquivo.write(', '.join(vendidos) + '\n')
+
+        if len(lista_cancel) > 0:
+            arquivo.write('Assentos cancelados: ')
+            arquivo.write(', '.join(lista_cancel) + '\n')
+
         arquivo.write('Assentos disponíveis: ' + ', '.join(assentos_disp) + '\n\n')
 
         arquivo.write("===================================================================\n"
-                      "Layout da posição dos assentos\n\n"
+                      "Layout atual da posição dos assentos\n\n"
                       "LEGENDA\n "
                       "\tM: Assento do motorista\n"
                       "\t▒: Assentos disponíveis para compra\n"
                       "\t▓: Assentos disponíveis para compra\n"
                       "\tX: Assentos indisponíveis para compra\n\n")
 
-        arquivo.write('\t\t' + '\t'.join(assentos.columns) +'\n')
+        arquivo.write('\t\t' + '\t'.join(assentos.columns) + '\n')
 
         for i in range(0, assentos.shape[0]):
             arquivo.write('\t' + str(i) + '\t')
-            for j in range(ord('A'), ord('A') + assentos.shape[1]):
+            for j in range(ord('A'), ord('A') + assentos.shape[1] -1):
                 arquivo.write(assentos.at[i, chr(j)] + '\t')
             arquivo.write('\n\n')
 
@@ -54,7 +59,7 @@ def escrever_txt(vendidos, assentos):
 # Essa função serve para renderizar a imagem que representa os acentos do ônibus
 def renderizar_imagem(assentos):
     strings = ''
-    strings += '\t\t' + '\t'.join(assentos.columns) +'\n'
+    strings += '\t\t' + '\t'.join(assentos.columns) + '\n'
 
     for i in range(0, assentos.shape[0]):
         strings += '\t' + str(i) + '\t'
@@ -66,7 +71,8 @@ def renderizar_imagem(assentos):
                 icone = '\033[37m▒\033[m'
             elif icone == '░':
                 icone = '\033[33m▒\033[m'
-            elif icone == '▓':                icone = '\033[32m▓\033[m'
+            elif icone == '▓':
+                icone = '\033[32m▓\033[m'
             elif icone == 'X':
                 icone = '\033[31mX\033[m'
             strings += icone + '\t'
@@ -81,6 +87,7 @@ def renderizar_imagem(assentos):
 
     print(strings)
 
+
 # cria e configura o layput/matriz dos assentos do ônibus conforme o usuário desejar
 def criar_matriz():
     num_linhas = int(input("Indique a quantidade de linhas: "))
@@ -90,8 +97,8 @@ def criar_matriz():
 
     corredores = []
     for i in range(num_corredores):
-        corredores.append(int(input(f"Indique o número da {i + 1}ª coluna do corredor vago de 0 a {num_colunas-1}: ")))
-
+        corredores.append(
+            int(input(f"Indique o número da {i + 1}ª coluna do corredor vago de 0 a {num_colunas - 1}: ")))
 
     matriz = []
     for i in range(num_linhas):
@@ -109,22 +116,65 @@ def criar_matriz():
                     linha.append('▓')
         matriz.append(linha)
 
-    assentos = pd.DataFrame(matriz, columns = criar_colunas(num_colunas))
+    assentos = pd.DataFrame(matriz, columns=criar_colunas(num_colunas))
 
     return assentos
 
+
 def criar_colunas(num_colunas):
     colunas = []
-    for i in range(ord('A') , ord('A') + num_colunas):
+    for i in range(ord('A'), ord('A') + num_colunas):
         colunas.append(chr(i))
-    return  colunas
+    return colunas
 
-def cancelar_compra():
-    pass
+
+def cancelar_compra(assentos, vendidos, lista_cancel):
+    print('##################### CANCELAR COMPRAS #######################\n')
+    renderizar_imagem(assentos)
+    try:
+        coord = input("\nindique a coordenada do assento desejado ou digite sair para finalizar: ").upper()
+
+        coord_x = coord[0]
+        coord_y = coord[1]
+        if coord[1].isdigit():
+            coord_y = int(coord[1])
+        else:
+            print('Coordenada do eixo horizontal inválida')
+
+        if coord == 'SAIR':
+            pass
+
+        else:
+            if len(coord) == 2:
+
+                if assentos.at[coord_y, coord_x] == 'X':
+                    assentos.at[coord_y, coord_x] = '▓'
+                    input('Compra cancelada. Aperte Enter para continuar...')
+                    ler_dados()
+                    assentos.to_csv('database.csv', index=False)
+                    try:
+                        vendidos.remove(coord)
+                    except:
+                        pass
+
+                    lista_cancel.append(coord)
+                    lista_cancel = pd.DataFrame(lista_cancel).transpose()
+                    lista_cancel.to_csv('lista_cancel.csv', index=False)
+                    if len(vendidos) > 0:
+                        vendidos = pd.DataFrame(vendidos).transpose()
+                        vendidos.to_csv('vendidos.csv', index=False)
+                else:
+                    input('A coordenada informada não é de um assento comprado. Aperte Enter para continuar...')
+            else:
+                input('A coordenada informada não é válida. Aperte Enter para continuar...')
+
+    except (IndexError, ValueError) as e:
+
+        print(f"Erro ao cancelar a compra: {e}")
+        input("Aperte qualquer tecla para continuar ...")
 
 
 def comprar(assentos, vendidos):
-
     # Selecionando o acento no ônibus. Neste momento o programa entra em um loop para que os assentos sejam selecionados
 
     while True:
@@ -207,46 +257,83 @@ def comprar(assentos, vendidos):
                 continue
             else:
                 break
-    if len(vendidos) > 0 :
+    if len(vendidos) > 0:
         vendidos = pd.DataFrame(vendidos).transpose()
         vendidos.to_csv('vendidos.csv', index=False)
         assentos.to_csv('database.csv', index=False)
-    print('parou aqui')
+
+
 def ler_dados():
     # Carregamento inicial do programa
 
     try:
-        vendidos = pd.read_csv('vendidos.csv', index_col=False)
-        vendidos = vendidos.values.tolist()[0]
+        lista_cancel = pd.read_csv('lista_cancel.csv', index_col=False)
+        if len(lista_cancel):
+            lista_cancel = lista_cancel.values.tolist()[0]
+        else:
+            lista_cancel = lista_cancel.values.tolist()
 
-    except FileNotFoundError as e:
+    except:
+        lista_cancel = pd.DataFrame([])
+        lista_cancel.to_csv('lista_cancel.csv', index=False)
+        lista_cancel = lista_cancel.values.tolist()
+
+    try:
+        vendidos = pd.read_csv('vendidos.csv', index_col=False)
+        if len(vendidos):
+            vendidos = vendidos.values.tolist()[0]
+        else:
+            vendidos = vendidos.values.tolist()
+
+
+
+    except:
         vendidos = pd.DataFrame([])
         vendidos.to_csv('vendidos.csv', index=False)
         vendidos = vendidos.values.tolist()
 
     try:
-        assentos = pd.read_csv('database.csv')
+        assentos = pd.read_csv('database.csv', index_col=False)
 
 
-    except FileNotFoundError as erro:
+    except:
 
         print('Arquivo não encontrado. Criando e configurando novo layout ...')
         assentos = criar_matriz()
         # criar csv
-        assentos.to_csv('database.csv')
+        assentos.to_csv('database.csv',  index=False)
 
+    return [assentos, vendidos, lista_cancel]
 
-    return [assentos, vendidos]
 
 def excluir_arquivo(file):
     if (os.path.exists(file) and os.path.isfile(file)):
         os.remove(file)
 
 
-def finalizar(assentos, vendidos):
+def finalizar(assentos, vendidos, lista_cancel):
     try:
-        escrever_txt(vendidos, assentos)
+        escrever_txt(vendidos, assentos, lista_cancel)
         print("Programa Finalizado. Dados registrados")
         excluir_arquivo('vendidos.csv')
+        excluir_arquivo('lista_cancel.csv')
     except:
         print("O programa finalizou mas os dados não foram registrados")
+
+
+def resetar():
+    opcao = input("Tem certeza que deseja resetar os dados S(sim)/N(não)?")
+
+    if opcao.isalpha():
+        opcao = opcao.upper()
+        if opcao == 'S':
+            assentos = criar_matriz()
+            # criar csv
+            assentos.to_csv('database.csv', index = False)
+            excluir_arquivo('relacao_assentos.txt')
+            input("Sistema resetado. Pressione Enter para continuar ...")
+        elif opcao == 'N':
+            print('Reset cancelado')
+
+    else:
+        print('Opção inválida')
